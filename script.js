2dae62925de4102d162c61ec7a36258252f0c95c
Zimanra <ramanzincristian@gmail.com>
2020-10-09 18:08:07 -0300

    Inicializar proyecto vue

---
+++ script.js
@@ -0,0 +1,17 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        //Acá ponemos las variables que vamos usar tanto en la vista
+        //como en las funciones. 
+    },
+    methods:{
+        //Acá definimos las funciones para los eventos https://es.vuejs.org/v2/guide/events.html#ad
+    },
+    mounted() {
+        //Todo lo que esté acá adentro se a ejecutar por única vez cuando 
+        //VUE renderice  todo el HTML, es decir, cuando inicia nuestra página.
+        //Acá va el fetch para traer los datos iniciales.
+        
+    },
+})
+
